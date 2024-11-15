09511e43416cc3626dd19256dac3ce2f3b463aaf
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2024-11-15 13:22:55 +0800

    mm

--- Activity.java
+++ Activity.java
@@ -1,14 +1,14 @@
 public class Activity {
     public static void main(String[] args) {
         
-        Student student = new Student("Juan", "Batangas PH", "BSIT", 2);
+        Student student = new Student("Patrick", "Balayan", "BSIT", 2);
         System.out.println("Student Name: " + student.getName());
         System.out.println("Student Address: " + student.getAddress());
         System.out.println("student Program: " + student.getProgram());
         System.out.println("Student Year: " + student.getYear());
         System.out.println(student.toString());
         
-        Staff staff = new Staff("Jaycee", "Pooc", "BSHS", 200000);
+        Staff staff = new Staff("Jaycee", "Pooc", "BSHS", 253000);
         System.out.println("Staff Name: " + staff.getName());
         System.out.println("Staff Address: " + staff.getAddress());
         System.out.println("Staff School: " + staff.getSchool());
