a2406814c348443d76449b85500968d3b63ffa7b
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2024-11-15 13:41:46 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main{
     public static void main(String[] args) {
-        Shape circle = new Circle(5.0, "red", true);
+        Shape circle = new Circle(45.0, "blue", true);
         System.out.println("\n" + "Area of Circle: " + circle.getArea());
         System.out.println("Perimeter of Circle: " +circle.getPerimeter());
         System.out.println(circle.toString() + "\n");
 
-        Shape Rectangle = new Rectangle(10.5, 5.5, "blue", true);
+        Shape Rectangle = new Rectangle(33.9, 90.0, "blue", true);
         System.out.println("Area of Rectangle: " + Rectangle.getArea());
         System.out.println("Perimeter of Rectangle: " + Rectangle.getPerimeter());
         System.out.println(Rectangle.toString() + "\n");
         
-        Shape Square = new Square(4.0, 5.0, 5.0, "mayumlue", true);
+        Shape Square = new Square(50.0, 55.0, 25.0, "blue", true);
         System.out.println("Area of Square: " + Square.getArea());
         System.out.println("Perimeter of Square: " + Square.getPerimeter());
         System.out.println(Square.toString());
